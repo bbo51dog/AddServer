f067d9136033c19d8bd739989f5450bc9307b17f
bbo51dog <bbo51@icloud.com>
2020-01-14 22:13:06 +0900

    Fix errors

--- js/AddServer.js
+++ js/AddServer.js
@@ -3,12 +3,12 @@ if(1 < location.search.length){
     var query = window.location.search.substring(1);
     var param = query.split('&');
     for(var i = 0; i < param.length; i++ ){
-        var element = parameters[i].split('=');
+        var element = param[i].split('=');
         var name = decodeURIComponent(element[ 0 ]);
         var value = decodeURIComponent(element[ 1 ]);
         get[name] = value;
     }
 }
-if(get['name'] and get['ip'] and get['port']){
+if(get['name'] && get['ip'] && get['port']){
     location.href = 'minecraft://?addExternalServer=' + get['name'] + '|' + get['ip'] + ':' + get['port'];
 }
