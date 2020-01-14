359f6560ac5f0425e09e5dfcc74fd89a177e6a4b
bbo51dog <bbo51@icloud.com>
2020-01-14 23:29:32 +0900

    onload

--- js/AddServer.js
+++ js/AddServer.js
@@ -1,15 +1,17 @@
-var get = {};
-if(1 < location.search.length){
-    var query = window.location.search.substring(1);
-    var param = query.split('&');
-    for(var i = 0; i < param.length; i++ ){
-        var element = param[i].split('=');
-        var name = decodeURIComponent(element[ 0 ]);
-        var value = decodeURIComponent(element[ 1 ]);
-        get[name] = value;
+function addServer(){
+    var get = {};
+    if(1 < location.search.length){
+        var query = window.location.search.substring(1);
+        var param = query.split('&');
+        for(var i = 0; i < param.length; i++ ){
+            var element = param[i].split('=');
+            var name = decodeURIComponent(element[ 0 ]);
+            var value = decodeURIComponent(element[ 1 ]);
+            get[name] = value;
+        }
     }
-}
-if(get['name'] && get['ip'] && get['port']){
-    alert('test');
-    location.href = 'minecraft://?addExternalServer=' + get['name'] + '|' + get['ip'] + ':' + get['port'];
+    if(get['name'] && get['ip'] && get['port']){
+        alert('test');
+        location.href = 'minecraft://?addExternalServer=' + get['name'] + '|' + get['ip'] + ':' + get['port'];
+    }
 }
