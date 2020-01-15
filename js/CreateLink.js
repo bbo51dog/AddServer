c3bfaff0a342da4fb919a459905c8c5b7c962dde
bbo51dog <bbo51@icloud.com>
2020-01-15 17:56:49 +0900

    Link

--- js/CreateLink.js
+++ js/CreateLink.js
@@ -1,6 +1,7 @@
 function createLink(){
     if(create_link.name.value && create_link.ip.value && create_link.port.value){
-        document.getElementById("result").value = "SYNCER"
+        var link = 'https://addserver.bbo51dog.net/?name=' + create_link.name.value + '&ip=' + create_link.ip.value + '&port=' + create_link.port.value;
+        document.getElementById("result").value = link;
         return true;
     }else{
         alert('サーバー名、IP、アドレスの全てを入力してください');
