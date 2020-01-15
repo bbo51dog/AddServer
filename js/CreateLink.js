24a99c469a2ebc61656b57f8e01aadace00c78aa
bbo51dog <bbo51@icloud.com>
2020-01-15 17:50:54 +0900

    result

--- js/CreateLink.js
+++ js/CreateLink.js
@@ -1,5 +1,6 @@
 function createLink(){
     if(create_link.name.value && create_link.ip.value && create_link.port.value){
+        document.getElementById("result").value = "SYNCER"
         return true;
     }else{
         alert('サーバー名、IP、アドレスの全てを入力してください');
