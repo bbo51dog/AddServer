5755d91913b68ba35a4840a87e0abca1fca77ee9
bbo51dog <bbo51dog@gmail.com>
2021-12-31 02:15:16 +0900

    Update AddServer.js

--- js/AddServer.js
+++ js/AddServer.js
@@ -13,6 +13,7 @@ function addServer() {
     if (get['name'] && get['ip'] && get['port']) {
         document.getElementById("transition_wrap").innerHTML =
             "<p>Minecraftを開いています<br>画面が切り替わらない場合はMinecraftがインストールされているかなどをご確認ください</p>" +
+            "<p>Minecraftへ遷移後にサーバーが追加されない場合があります。(原因調査中)" +
             "<a href='/'>TOPへ戻る</a>"
         location.href = 'minecraft://?addExternalServer=' + get['name'] + '|' + get['ip'] + ':' + get['port'];
     }
